821e108d3bb733ee80302fb9d94fdb570ae0426b
jrossdelacruz <2015081632@ust-ics.mygbiz.com>
2018-11-15 23:32:56 +0800

    Initial commit

---
+++ app/src/main/java/johnross/delacruz/com/finalsproject_delacruz_han/MainActivity.java
@@ -0,0 +1,13 @@
+package johnross.delacruz.com.finalsproject_delacruz_han;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
