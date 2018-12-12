176f92f0f66e96a90e0b9c2fd5d99f962a82d9aa
jrossdelacruz <2015081632@ust-ics.mygbiz.com>
2018-12-12 18:47:54 +0800

    finished vegetable category (cabbage)

--- app/src/main/java/johnross/delacruz/com/finalsproject_delacruz_han/CabbageHowToFragment.java
+++ app/src/main/java/johnross/delacruz/com/finalsproject_delacruz_han/CabbageHowToFragment.java
@@ -7,11 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class CabbageHowToFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cabbage_howto, container, false);
+        View v = inflater.inflate(R.layout.fragment_cabbage_howto, container, false);
+
+        TextView planting = v.findViewById(R.id.cabbage_planting);
+        planting.setText(R.string.planting_cabbage);
+
+        TextView care = v.findViewById(R.id.cabbage_care);
+        care.setText(R.string.care_cabbage);
+
+        return v;
     }
 }
--- app/src/main/java/johnross/delacruz/com/finalsproject_delacruz_han/CabbageTipsFragment.java
+++ app/src/main/java/johnross/delacruz/com/finalsproject_delacruz_han/CabbageTipsFragment.java
@@ -7,11 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class CabbageTipsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cabbage_tips, container, false);
+        View v = inflater.inflate(R.layout.fragment_cabbage_tips, container, false);
+
+        TextView pests = v.findViewById(R.id.cabbage_pests);
+        pests.setText(R.string.pests_cabbage);
+
+        TextView harvest = v.findViewById(R.id.cabbage_harvest);
+        harvest.setText(R.string.harvest_cabbage);
+
+        return v;
     }
 }
