e1c49fe9aa089ca040219024c7afebeea2ee2ee2
jrossdelacruz <2015081632@ust-ics.mygbiz.com>
2018-12-12 20:29:22 +0800

    finished fruits category (blueberries)

--- app/src/main/java/johnross/delacruz/com/finalsproject_delacruz_han/BlueberriesHowToFragment.java
+++ app/src/main/java/johnross/delacruz/com/finalsproject_delacruz_han/BlueberriesHowToFragment.java
@@ -7,11 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class BlueberriesHowToFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_blueberries_howto, container, false);
+        View v = inflater.inflate(R.layout.fragment_blueberries_howto, container, false);
+
+        TextView planting = v.findViewById(R.id.blueberries_planting);
+        planting.setText(R.string.planting_blueberries);
+
+        TextView care = v.findViewById(R.id.blueberries_care);
+        care.setText(R.string.care_blueberries);
+
+        return v;
     }
 }
--- app/src/main/java/johnross/delacruz/com/finalsproject_delacruz_han/BlueberriesTipsFragment.java
+++ app/src/main/java/johnross/delacruz/com/finalsproject_delacruz_han/BlueberriesTipsFragment.java
@@ -7,11 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class BlueberriesTipsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_blueberries_tips, container, false);
+        View v = inflater.inflate(R.layout.fragment_blueberries_tips, container, false);
+
+        TextView pests = v.findViewById(R.id.blueberries_pests);
+        pests.setText(R.string.pests_blueberries);
+
+        TextView harvest = v.findViewById(R.id.blueberries_harvest);
+        harvest.setText(R.string.harvest_blueberries);
+
+        return v;
     }
 }
